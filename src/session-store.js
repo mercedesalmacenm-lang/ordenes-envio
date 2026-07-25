3f50dc49d737d72e9d76425c71ca90fa8fe08218
OE-Admin <mercedesalmacenm-lang@users.noreply.github.com>
2026-07-25 16:09:08 -0700

    fix: guard all session store callbacks against missing function

--- src/session-store.js
+++ src/session-store.js
@@ -10,6 +10,7 @@ function SupabaseSessionStore() {
 util.inherits(SupabaseSessionStore, Store);
 
 SupabaseSessionStore.prototype.get = function (sid, callback) {
+  if (typeof callback !== 'function') return;
   supabase.from(this.table).select('sess').eq('sid', sid).single()
     .then(({ data, error }) => {
       if (error || !data) return callback(null, null);
@@ -24,6 +25,7 @@ SupabaseSessionStore.prototype.get = function (sid, callback) {
 };
 
 SupabaseSessionStore.prototype.set = function (sid, session, callback) {
+  if (typeof callback !== 'function') return;
   try {
     const sess = JSON.parse(JSON.stringify(session));
     const expires = sess.cookie && sess.cookie.expires
@@ -48,12 +50,14 @@ SupabaseSessionStore.prototype.set = function (sid, session, callback) {
 };
 
 SupabaseSessionStore.prototype.destroy = function (sid, callback) {
+  const cb = typeof callback === 'function' ? callback : function () {};
   supabase.from(this.table).delete().eq('sid', sid)
-    .then(() => callback(null))
-    .catch(() => callback(null));
+    .then(() => cb(null))
+    .catch(() => cb(null));
 };
 
 SupabaseSessionStore.prototype.touch = function (sid, session, callback) {
+  const cb = typeof callback === 'function' ? callback : function () {};
   try {
     const sess = JSON.parse(JSON.stringify(session));
     const expires = sess.cookie && sess.cookie.expires
@@ -62,10 +66,10 @@ SupabaseSessionStore.prototype.touch = function (sid, session, callback) {
     supabase.from(this.table)
       .update({ expire: expires.toISOString() })
       .eq('sid', sid)
-      .then(() => callback(null))
-      .catch(() => callback(null));
+      .then(() => cb(null))
+      .catch(() => cb(null));
   } catch (e) {
-    callback(null);
+    cb(null);
   }
 };
 
